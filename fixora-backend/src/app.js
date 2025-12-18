f7d361a693ab3c3c319d3334b98f3486900bef92
Jaiveer2004 <singhjaiveer1005@gmail.com>
2025-12-18 20:19:44 +0530

    Some more minor changes

--- fixora-backend/src/app.js
+++ fixora-backend/src/app.js
@@ -24,7 +24,7 @@ const app = express();
 
 // Middlewares:
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: process.env.FRONTEND_URL,
   credentials: true
 }))
 
